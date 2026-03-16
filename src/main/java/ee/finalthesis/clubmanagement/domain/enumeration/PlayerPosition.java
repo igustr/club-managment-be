27ac8336573d302a19cf7782e35313bf2908ee15
igorustritski <igustr@taltech.ee>
2026-03-16 15:33:43 +0200

    seed data, surface type, player position part 2

---
+++ src/main/java/ee/finalthesis/clubmanagement/domain/enumeration/PlayerPosition.java
@@ -0,0 +1,8 @@
+package ee.finalthesis.clubmanagement.domain.enumeration;
+
+public enum PlayerPosition {
+  GOALKEEPER,
+  DEFENDER,
+  MIDFIELDER,
+  STRIKER,
+}
---
+++ src/main/java/ee/finalthesis/clubmanagement/domain/enumeration/SurfaceType.java
@@ -0,0 +1,7 @@
+package ee.finalthesis.clubmanagement.domain.enumeration;
+
+public enum SurfaceType {
+  NATURAL_GRASS,
+  ARTIFICIAL_TURF,
+  INDOOR,
+}
